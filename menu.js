c24a43eba0eaf4e6effb7fbc7c0077c9c052fa82
Carlos7e6 <carlos.gomez.7e6@itb.cat>
2023-05-07 20:16:42 +0200

    La puta basura esta de trabajo GetJugador hecho pocho pero va

--- menu.js
+++ menu.js
@@ -3,12 +3,26 @@ const UI = new UIManager(cerebro.getNumLevels());
 const conexionBase = 'http://rolu.sytes.net:7053/';
 
 var nombre_Jugador = localStorage.getItem("nombre_Jugador");
+localStorage.removeItem("nombre_Jugador");
 
-// const Jugador_Data = cerebro.getJugadorInfo(nombre_Jugador);
+if(nombre_Jugador == null) window.location.replace("index.html");
 
-const jugador = new Jugador("admin",1,"Barcelona")
 
-UI.printarMenu();
+let Jugador_Data;
+let usuario;
+
+getJugadorInfo();
+
+setTimeout(() => {
+    usuario = new Jugador(Jugador_Data.nombre_Jugador,Jugador_Data.nivel_actual,Jugador_Data.ciudad);   
+    UI.printarMenu();
+  }, 100);
+
+
+async function getJugadorInfo(/* string */) {
+    const datos = await cerebro.getJugadorInfo(nombre_Jugador);
+    Jugador_Data =  datos;
+}
 
 function printarRanking(){
     UI.printarRanking();
@@ -31,5 +45,5 @@ function printarNivel()
 
 function printarNiveles()
 {
-    UI.printarNiveles(jugador);
+    UI.printarNiveles(usuario);
 }
